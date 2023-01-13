e10d133da40241a8812fdd126897cfe33c2cc4dc
Bayarkhuu <bayarkhuu.srs@gmail.com>
2023-01-13 20:03:33 +0800

    report

--- src/views/auth/dao/AuthDAO.ts
+++ src/views/auth/dao/AuthDAO.ts
@@ -1,7 +1,13 @@
 import axios from "../../../utils/AxiosInstance";
 import {TokenDto} from "../models/AuthModels";
+import {toast} from "react-toastify";
 
 export async function login(username: string, password: string) {
-    const request = await axios.post<TokenDto>("/auth/login", {username, password});
-    return request.data;
+    try {
+        const request = await axios.post<TokenDto>("/auth/login", {username, password});
+        return request.data;
+    } catch (e) {
+        toast.error("Нэвтрэх нэр эсвэл нууц үг буруу байна.");
+    }
+
 }
